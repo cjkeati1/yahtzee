de94a25ed5c2a502dd5b81050f6cfb6883efc0bb
Connor Keating <cjkeati1@asu.edu>
2019-12-28 12:10:19 -0700

    made RuleRow component stateless. The game can now restart

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -2,23 +2,14 @@ import React, {Component} from 'react';
 import './RuleRow.css'
 
 class RuleRow extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {completed: false};
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick() {
-        this.setState({completed: true});
-        this.props.doScore();
-    }
-
     render() {
+        const completed = this.props.score !== undefined;
+
         return (
-            <tr className={`RuleRow ${this.state.completed ? 'RuleRow-disabled' : 'RuleRow-active'} ${!this.props.hasStarted || this.props.isRolling ? 'RuleRow-unclickable' : ''}`}
-                onClick={this.state.completed || !this.props.hasStarted || this.props.isRolling ? null : this.handleClick}>
+            <tr className={`RuleRow ${completed ? 'RuleRow-disabled' : 'RuleRow-active'} ${!this.props.hasStarted || this.props.isRolling ? 'RuleRow-unclickable' : ''}`}
+                onClick={completed || !this.props.hasStarted || this.props.isRolling ? null : this.props.doScore}>
                 <td className="RuleRow-name">{this.props.name}</td>
-                <td className="RuleRow-score">{this.state.completed ? this.props.score : this.props.description}</td>
+                <td className="RuleRow-score">{completed ? this.props.score : this.props.description}</td>
             </tr>
         )
     }
